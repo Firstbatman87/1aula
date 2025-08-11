a7362260f3c81dc567c4ff987251e99ddacc2718
Firstbatman87 <sidneymiguel999@gmail.com>
2025-08-11 17:16:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -35,4 +35,57 @@
         echo "A é maior que B!";
     }
     echo "<br>Saiu do IF";
+    echo "<hr>" ;
+    $n1 = 13;
+    $n2 = 1;
+    $n3 = 8;
+    if ($n1 > $n2 && $n1 > $n3) {
+        echo "A variavel " .$n1. " é o valor maior! ";
+
+    }
+    elseif ($n2 > $n1 && $n2 > $n3) {
+        echo "A variavel " .$n2. " é o valor maior!";
+    }
+    else {
+        echo "A variavel " .$n3. "é o valor maior!";
+    }
+    echo "<hr>";
+    echo "<center><h2>NÚMEROS RAMDOMICOS</h2></center>";
+    $num = rand(1, 10);
+    echo " O número sorteado foi: " .$num;
+    $nm = 5;
+    if ($nm == $num) {
+        echo "<br>Seu número é: " .$nm; 
+        echo "<br>Você ganhou o sorteio";
+    } else {
+        echo "<br>Não foi dessa vez ";
+    }
+    echo "<hr>";
+    for ($i=0; $i < 10; $i++) {
+        echo "Passagem do laço for nº:" .$i. "<br>";
+    }
+    echo "<hr>";
+    echo "<h2>TABUADA</h2>";
+    $numtab = 5;
+    for ($i=1; $i <= 10; $i++) {
+        echo $numtab. " X " .$i. " = " .$numtab*$i. "<br>";
+    }
+    echo "<hr>";
+    echo "<h2>Laço WHILE</h2>";
+    $x = 1;
+    while ($x < 6) {
+        echo "Laço nº:" .$x. "<br>";
+        $x++;
+    }
+    $a = 0;
+    while ($a < 100) {
+        echo $a. "<br>";
+        $a+=10;
+    }
+    echo "<hr>";
+    echo "<h2>FOREACH</h2>";
+    $cores = array("azul", "marrom", "bege", "amarelo", "branco");
+    foreach ($cores as $cor) {
+        echo $cor. "<br>";
+    }
 ?>
